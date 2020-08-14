2b2a42d98a9271679e28a2bf4c59d53f32d87856
RaisanJMR <raisanjmr@protonmail.com>
2020-08-14 09:46:39 +0530

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var form = document.getElementById("addForm");
 var itemList = document.getElementById("items");
+var filter = document.getElementById("filter");
 
 // Add Submit Event
-form.addEventListener("submit", addItem);
+form.addEventListener("submit", addItems);
+// delete event
+itemList.addEventListener("click", removeItems);
+// filter event
+filter.addEventListener("keyup", filterItems);
 
 // Add Item
-function addItem(e) {
+function addItems(e) {
   e.preventDefault();
 
   // Get input Value
@@ -34,4 +39,31 @@ function addItem(e) {
 
   //   adding li to ul
   itemList.appendChild(li);
+}
+
+// Remove item
+function removeItems(e) {
+  // delete event should only happen when we click delete button
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are you sure!")) {
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+// Filter item
+function filterItems(e) {
+  // convert to lowercase
+  var text = e.target.value.toLowerCase();
+  //   Get li's
+  var items = itemList.getElementsByTagName("li");
+  Array.from(items).forEach(function (item) {
+    var itemName = item.firstChild.textContent;
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
